d940638ac7702f78a5cb93d69b4a6c73ad4c58f0
oomugiwakaba <76947273+oomugiwakaba@users.noreply.github.com>
2021-02-27 16:16:37 +0900

    提出２

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    uname {Faker::Name.initials(number: 2)}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    birthday           {"1996-11-16"}
+    sei                {"大田"}
+    mei                {"義夫"}
+    sei_huri           {"オオタ"}
+    mei_huri           {"ヨシオ"}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it 'emailが空では登録できないこと' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'emailが一意性でないと登録できないこと' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+
+    it 'emailに@がないと登録できないこと' do
+      @user.email = 'sample.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
+    end
+
+    it 'passwordが空では登録できないこと' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'passwordが5文字以下では登録できないこと' do
+      @user.password = '12345'
+      @user.password_confirmation = '12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
+    it 'passwordと確認用passwordが一致しないと登録できないこと' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+
+
+
+  end
+end
