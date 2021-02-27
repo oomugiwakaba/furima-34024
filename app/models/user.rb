8468908098482bb60bc154166ac76d5e39a85488
oomugiwakaba <76947273+oomugiwakaba@users.noreply.github.com>
2021-02-27 21:33:15 +0900

    提出３

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :uname, presence: true
-  validates :birthday, presence: true
-  validates :sei, presence: true
-  validates :mei, presence: true
-  validates :sei_huri, presence: true
-  validates :mei_huri, presence: true
+  with_options presence: true do
+    validates :uname
+    validates :birthday
+    validates :sei, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :mei, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :sei_huri, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :mei_huri, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :password_confirmation, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+
+  end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,45 +6,139 @@ RSpec.describe User, type: :model do
       @user = FactoryBot.build(:user)
     end
 
-    it 'emailが空では登録できないこと' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+
+    context 'ユーザー新規登録ができる時' do
+
+      it 'パスワードは、6文字以上での入力と半角英数字混合での入力で登録可能であること' do
+        @user.password = '1asdfg'
+        @user.password_confirmation = '1asdfg'
+        expect(@user).to be_valid
+      end
+
+
+
     end
 
-    it 'emailが一意性でないと登録できないこと' do
-      @user.save
-      another_user = FactoryBot.build(:user, email: @user.email)
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
-    end
 
-    it 'emailに@がないと登録できないこと' do
-      @user.email = 'sample.com'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email is invalid')
-    end
+    context 'ユーザー新規登録ができない時' do
 
-    it 'passwordが空では登録できないこと' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
+      it 'ニックネームが必須であること' do
+        @user.uname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Uname can't be blank")
+      end
 
-    it 'passwordが5文字以下では登録できないこと' do
-      @user.password = '12345'
-      @user.password_confirmation = '12345'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-    end
+      it 'emailが空では登録できないこと' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+  
+      it 'emailが一意性でないと登録できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+  
+      it 'emailに@がないと登録できないこと' do
+        @user.email = 'sample.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+  
+      it 'passwordが空では登録できないこと' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+  
+      it 'passwordが5文字以下では登録できないこと' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+  
+      it 'passwordと確認用passwordが一致しないと登録できないこと' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
 
-    it 'passwordと確認用passwordが一致しないと登録できないこと' do
-      @user.password = '123456'
-      @user.password_confirmation = '1234567'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'パスワードは、半角英数字混合での入力が必須であること' do
+        @user.password = 'aaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
 
+      it 'seiが空では登録できないこと' do
+        @user.sei = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Sei can't be blank", "Sei is invalid. Input full-width characters.")
+      end
+
+      it 'meiが空では登録できないこと' do
+        @user.mei = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Mei can't be blank", "Mei is invalid. Input full-width characters.")
+      end
+
+      it 'seiは、全角（漢字・ひらがな・カタカナ）でないと登録できないこと' do
+        @user.sei = 'fdhr'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Sei is invalid. Input full-width characters.")
+      end
+
+      it 'meiは、全角（漢字・ひらがな・カタカナ）でないと登録できないこと' do
+        @user.mei = 'hiydjt'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Mei is invalid. Input full-width characters.")
+      end
+
+      it 'sei_huriが空では登録できないこと' do
+        @user.sei_huri = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Sei huri can't be blank", "Sei huri is invalid. Input full-width katakana characters.")
+      end
+
+      it 'mei_huriが空では登録できないこと' do
+        @user.mei_huri = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Mei huri can't be blank", "Mei huri is invalid. Input full-width katakana characters.")
+      end
+
+      it 'sei_huriは、全角（カタカナ）でないと登録できないこと' do
+        @user.sei_huri = 'ostw'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Sei huri is invalid. Input full-width katakana characters.")
+      end
+
+      it 'mei_huriは、全角（カタカナ）でないと登録できないこと' do
+        @user.mei_huri = 'hrshh'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Mei huri is invalid. Input full-width katakana characters.")
+      end
+
+      it '生年月日が必須であること' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+
+
+
+
+
+
+    end
 
 
 
