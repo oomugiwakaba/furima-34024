a17c7630879c1c1d9ce6420439c328d4d643dbfb
oomugiwakaba <76947273+oomugiwakaba@users.noreply.github.com>
2021-03-13 12:33:26 +0900

    商品情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,12 @@ class ItemsController < ApplicationController
 
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-    @items = Item.new(item_params)
-    if @items.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -23,6 +23,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+
+
+  end
 
   private
 
